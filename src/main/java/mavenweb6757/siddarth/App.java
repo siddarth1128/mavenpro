38c6d42ad188bf22dca017ccd549ab6990194681
siddarth1128 <imsiddarth28@gmail.com>
2025-12-02 19:45:12 +0530

    sample maven project

---
+++ src/main/java/mavenweb6757/siddarth/App.java
@@ -0,0 +1,13 @@
+package mavenweb6757.siddarth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
